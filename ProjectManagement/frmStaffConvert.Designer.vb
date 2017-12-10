19567913b10e155516bc0e0706d6db2abbbd87d7
Yuya Maruyama <cir9le@gmail.com>
2017-12-10 16:24:21 +0900

    Create Staff Convert Form with codes [Section 13]

---
+++ ProjectManagement/frmStaffConvert.Designer.vb
@@ -0,0 +1,99 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmStaffConvert
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.cmbStaffCodeBefore = New System.Windows.Forms.ComboBox()
+        Me.cmbStaffCodeAfter = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnExec = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'cmbStaffCodeBefore
+        '
+        Me.cmbStaffCodeBefore.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cmbStaffCodeBefore.FormattingEnabled = True
+        Me.cmbStaffCodeBefore.Location = New System.Drawing.Point(65, 23)
+        Me.cmbStaffCodeBefore.Name = "cmbStaffCodeBefore"
+        Me.cmbStaffCodeBefore.Size = New System.Drawing.Size(121, 20)
+        Me.cmbStaffCodeBefore.TabIndex = 0
+        '
+        'cmbStaffCodeAfter
+        '
+        Me.cmbStaffCodeAfter.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cmbStaffCodeAfter.FormattingEnabled = True
+        Me.cmbStaffCodeAfter.Location = New System.Drawing.Point(65, 64)
+        Me.cmbStaffCodeAfter.Name = "cmbStaffCodeAfter"
+        Me.cmbStaffCodeAfter.Size = New System.Drawing.Size(121, 20)
+        Me.cmbStaffCodeAfter.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(14, 26)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(41, 12)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "変更前"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(14, 65)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(41, 12)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "変更後"
+        '
+        'btnExec
+        '
+        Me.btnExec.Location = New System.Drawing.Point(220, 21)
+        Me.btnExec.Name = "btnExec"
+        Me.btnExec.Size = New System.Drawing.Size(75, 23)
+        Me.btnExec.TabIndex = 4
+        Me.btnExec.Text = "実行"
+        Me.btnExec.UseVisualStyleBackColor = True
+        '
+        'frmStaffConvert
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(315, 105)
+        Me.Controls.Add(Me.btnExec)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.cmbStaffCodeAfter)
+        Me.Controls.Add(Me.cmbStaffCodeBefore)
+        Me.Name = "frmStaffConvert"
+        Me.Text = "frmStaffConvert"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents cmbStaffCodeBefore As ComboBox
+    Friend WithEvents cmbStaffCodeAfter As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnExec As Button
+End Class
---
+++ ProjectManagement/frmStaffConvert.vb
@@ -0,0 +1,90 @@
+﻿Public Class frmStaffConvert
+    Private Sub frmStaffConvert_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim sql As String
+
+        'SQLステートメントを指定する
+        sql = "SELECT staff_code, staff_name FROM tbl_staff ORDER BY staff_code"
+
+        'コネクションを指定する
+        Using connection As New SqlClient.SqlConnection(
+            My.Settings.project_jobConnectionString)
+
+            'データコマンドを定義する
+            Dim command As New SqlClient.SqlCommand(sql, connection)
+
+            'コネクションを開く
+            connection.Open()
+
+            'データリーダーの生成
+            Dim dr As SqlClient.SqlDataReader = command.ExecuteReader()
+
+            'データが存在する場合に、コンボボックスに項目を追加する
+            Do While dr.Read
+                cmbStaffCodeBefore.Items.Add(dr("staff_code") & ":" & dr("staff_name"))
+                cmbStaffCodeAfter.Items.Add(dr("staff_code") & ":" & dr("staff_name"))
+            Loop
+
+            'データリーダーを閉じる
+            dr.Close()
+
+            'コネクションを閉じる
+            connection.Close()
+
+        End Using
+    End Sub
+
+    Private Sub btnExec_Click(sender As Object, e As EventArgs) Handles btnExec.Click
+        Dim stcode_before As String, stcode_after As String
+
+        'コンボボックスでスタッフが指定されていない場合は処理を中断する
+        If cmbStaffCodeBefore.Text = "" Then
+            MsgBox("変更前のスタッフを指定してください")
+            Return
+        End If
+
+        If cmbStaffCodeAfter.Text = "" Then
+            MsgBox("変更後のスタッフを指定してください")
+            Return
+        End If
+
+        'スタッフコードの取得（先頭6文字）
+        stcode_before = Strings.Left(cmbStaffCodeBefore.Text, 6)
+        stcode_after = Strings.Left(cmbStaffCodeAfter.Text, 6)
+
+        '変更前と変更後のスタッフコードが同じ場合には処理を中断する
+        If stcode_before = stcode_after Then
+            MsgBox("異なるスタッフを選択してください")
+            Return
+        End If
+
+        'コネクションを指定する
+        Using connection As New SqlClient.SqlConnection(
+            My.Settings.project_jobConnectionString)
+
+            'データコマンドを作成する
+            Dim command As SqlClient.SqlCommand = connection.CreateCommand()
+
+            'コネクションを開く
+            connection.Open()
+
+            'データコマンドの定義と実行
+            'tbl_projectテーブルの処理
+            command.CommandText =
+                "UPDATE tbl_project SET staff_code='" & stcode_after & "' " &
+                "WHERE staff_code='" & stcode_before & "' AND complete_flag=0"
+            command.ExecuteNonQuery()
+
+            'tbl_jobテーブルの処理
+            command.CommandText =
+                "UPDATE tbl_job SET staff_code='" & stcode_after & "' " &
+                "WHERE staff_code='" & stcode_before & "' AND complete_flag=0"
+            command.ExecuteNonQuery()
+
+            MsgBox("プロジェクトとジョブのスタッフを変更しました")
+
+            'コネクションを閉じる
+            connection.Close()
+
+        End Using
+    End Sub
+End Class
