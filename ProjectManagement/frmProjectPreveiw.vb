40b4bb174b69457fb7ed061155b60fa19d1fb7be
Yuya Maruyama <cir9le@gmail.com>
2017-12-23 17:51:27 +0900

    Create Report Preveiw Form [Section 14]

---
+++ ProjectManagement/frmProjectPreveiw.vb
@@ -0,0 +1,8 @@
+﻿Public Class frmProjectPreveiw
+    Private Sub frmProjectPreveiw_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: このコード行はデータを 'project_jobDataSet.vw_projectlist' テーブルに読み込みます。必要に応じて移動、または削除をしてください。
+        Me.vw_projectlistTableAdapter.Fill(Me.project_jobDataSet.vw_projectlist)
+
+        Me.ReportViewer1.RefreshReport()
+    End Sub
+End Class
