110817591e03fae7dcbe79fc383edc9f82d66335
Yuya Maruyama <cir9le@gmail.com>
2017-12-23 18:25:07 +0900

    Create Report Print Form [Section 14]

---
+++ ProjectManagement/frmProjectPrint.Designer.vb
@@ -0,0 +1,123 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmProjectPrint
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnPrint = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.chkComplete = New System.Windows.Forms.CheckBox()
+        Me.cmbManager = New System.Windows.Forms.ComboBox()
+        Me.cmbCustomer = New System.Windows.Forms.ComboBox()
+        Me.btnCancel = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(62, 12)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "マネージャー"
+        '
+        'btnPrint
+        '
+        Me.btnPrint.Location = New System.Drawing.Point(211, 6)
+        Me.btnPrint.Name = "btnPrint"
+        Me.btnPrint.Size = New System.Drawing.Size(75, 23)
+        Me.btnPrint.TabIndex = 2
+        Me.btnPrint.Text = "印刷"
+        Me.btnPrint.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 47)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(29, 12)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "顧客"
+        '
+        'chkComplete
+        '
+        Me.chkComplete.AutoSize = True
+        Me.chkComplete.Location = New System.Drawing.Point(24, 74)
+        Me.chkComplete.Name = "chkComplete"
+        Me.chkComplete.Size = New System.Drawing.Size(107, 16)
+        Me.chkComplete.TabIndex = 6
+        Me.chkComplete.Text = "完了データを含む"
+        Me.chkComplete.UseVisualStyleBackColor = True
+        '
+        'cmbManager
+        '
+        Me.cmbManager.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cmbManager.FormattingEnabled = True
+        Me.cmbManager.Location = New System.Drawing.Point(80, 8)
+        Me.cmbManager.Name = "cmbManager"
+        Me.cmbManager.Size = New System.Drawing.Size(121, 20)
+        Me.cmbManager.TabIndex = 7
+        '
+        'cmbCustomer
+        '
+        Me.cmbCustomer.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cmbCustomer.FormattingEnabled = True
+        Me.cmbCustomer.Location = New System.Drawing.Point(80, 44)
+        Me.cmbCustomer.Name = "cmbCustomer"
+        Me.cmbCustomer.Size = New System.Drawing.Size(121, 20)
+        Me.cmbCustomer.TabIndex = 9
+        '
+        'btnCancel
+        '
+        Me.btnCancel.Location = New System.Drawing.Point(211, 42)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(75, 23)
+        Me.btnCancel.TabIndex = 8
+        Me.btnCancel.Text = "キャンセル"
+        Me.btnCancel.UseVisualStyleBackColor = True
+        '
+        'frmProjectView
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(298, 98)
+        Me.Controls.Add(Me.cmbCustomer)
+        Me.Controls.Add(Me.btnCancel)
+        Me.Controls.Add(Me.cmbManager)
+        Me.Controls.Add(Me.chkComplete)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.btnPrint)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "frmProjectView"
+        Me.Text = "プロジェクト印刷一覧"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnPrint As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents chkComplete As CheckBox
+    Friend WithEvents cmbManager As ComboBox
+    Friend WithEvents cmbCustomer As ComboBox
+    Friend WithEvents btnCancel As Button
+End Class
---
+++ ProjectManagement/frmProjectPrint.vb
@@ -0,0 +1,3 @@
+﻿Public Class frmProjectPrint
+
+End Class
